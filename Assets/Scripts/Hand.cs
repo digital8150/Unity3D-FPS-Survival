81243a69cb20f2b8b6200b3fec2cdbfb86c41cad
digital8150 <wptpgud@gmail.com>
2025-06-29 19:11:43 +0900

    featue: 팔 구현

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public string handName;
+    public float range; //공격 범위
+    public int damage;
+    public float workSpeed; //작업속도
+    public float attackDelay; //공격 딜레이
+    public float attackDelayA; //공격 활성화 시점
+    public float attackDelayB; //공격 비활성화 시점
+
+    public Animator anim;
+    
+}
